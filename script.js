116d24217d631f93da89a612a6066c9e1f9d7842
Ilia <umbomaxia@gmail.com>
2022-04-27 23:02:55 +0300

    feat: add spinner

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const dates = document.querySelectorAll('input[name="date"]');
 const firstDate = document.getElementById("firstdate");
 const secondDate = document.getElementById("seconddate");
 const submitBtn = document.getElementById("submit");
-const minField = document.querySelector('.min')
-const maxField = document.querySelector('.max')
-
-const DAY_IN_MILISECONDS = 24 * 60 * 60 * 1000
+const minField = document.querySelector(".min");
+const maxField = document.querySelector(".max");
+const spinner = document.querySelector('.spin-wrapper')
+const DAY_IN_MILISECONDS = 24 * 60 * 60 * 1000;
 
 let firstDateValue;
 let secondDateValue;
@@ -24,64 +24,70 @@ firstDate.max = secondDate.value;
 
 dates.forEach((el) =>
   el.addEventListener("input", () => {
+    submitBtn.disabled = true
     if (!firstDate.value || !secondDate.value) {
       return;
     }
 
     firstDateValue = new Date(firstDate.value).getTime();
     secondDateValue = new Date(secondDate.value).getTime();
-    console.log(firstDateValue);
-    console.log(new Date(firstDateValue));
+    if (secondDateValue - firstDateValue > 732 * DAY_IN_MILISECONDS) {
+      return;
+    }
     firstDateValue < secondDateValue ? (submitBtn.disabled = false) : (submitBtn.disabled = true);
   })
 );
 
-submitBtn.addEventListener("click", async() => {
+submitBtn.addEventListener("click", () => {
+  spinner.classList.add('visible')
   const arr = getArrayOfDates(firstDateValue, secondDateValue);
   const objOfDates = {};
   firstDate.disabled = true;
   secondDate.disabled = true;
-  submitBtn.disabled = true
-  await Promise.all(arr.map((el) => fetch(`https://www.nbrb.by/api/exrates/rates/USD?parammode=2&periodicity=0&ondate=${el}`)))
-  .then((res) =>{
-    return Promise.all(res.map(r => r.json()))})
-  .then((res) => {
-      res.forEach(el=>{
-        objOfDates[el.Date.slice(0,10)] = el.Cur_OfficialRate 
-      })
-      
-      let maxCur = {'date':0}
-      let minCur = {'date': Number.MAX_SAFE_INTEGER}
-      for(let key in objOfDates){
+  submitBtn.disabled = true;
+  Promise.all(arr.map((el) => fetch(`https://www.nbrb.by/api/exrates/rates/USD?parammode=2&periodicity=0&ondate=${el}`)))
+    .then((res) => {
+      return Promise.all(res.map((r) => r.json()));
+    })
+    .then((res) => {
+      res.forEach((el) => {
+        objOfDates[el.Date.slice(0, 10)] = el.Cur_OfficialRate;
+      });
 
-            // конвертируем валюту до деноминации 
-            if(objOfDates[key]>100){
-                objOfDates[key] = objOfDates[key]/10000
-            }
+      let maxCur = { date: 0 };
+      let minCur = { date: Number.MAX_SAFE_INTEGER };
+      for (let key in objOfDates) {
+        // конвертируем валюту до деноминации
+        if (objOfDates[key] > 100) {
+          objOfDates[key] = objOfDates[key] / 10000;
+        }
 
-          if(objOfDates[key]>maxCur[Object.keys(maxCur)[0]]){
-            let oldKey = Object.keys(maxCur)[0]
-            delete Object.assign(maxCur, {[key]: maxCur[oldKey] })[oldKey];
-            maxCur[key] = objOfDates[key]
-          }
-          if(objOfDates[key]<minCur[Object.keys(minCur)[0]]){
-            let oldKey = Object.keys(minCur)[0]
-            delete Object.assign(minCur, {[key]: minCur[oldKey] })[oldKey];
-            minCur[key] = objOfDates[key]
-          }
-        
+        if (objOfDates[key] > maxCur[Object.keys(maxCur)[0]]) {
+          let oldKey = Object.keys(maxCur)[0];
+          delete Object.assign(maxCur, { [key]: maxCur[oldKey] })[oldKey];
+          maxCur[key] = objOfDates[key];
+        }
+        if (objOfDates[key] < minCur[Object.keys(minCur)[0]]) {
+          let oldKey = Object.keys(minCur)[0];
+          delete Object.assign(minCur, { [key]: minCur[oldKey] })[oldKey];
+          minCur[key] = objOfDates[key];
+        }
       }
-      createText(maxCur, maxField)
-      createText(minCur, minField)
-
-
+      createText(maxCur, maxField);
+      createText(minCur, minField);
+      spinner.classList.remove('visible')
       firstDate.disabled = false;
       secondDate.disabled = false;
-      submitBtn.disabled = false
-  });
-
-  //   //   https://www.nbrb.by/api/exrates/rates?periodicity=0&ondate=${date}
-//   then(responses => Promise.all(responses.map(r => r.json())))
+      submitBtn.disabled = false;
+    })
+    .catch(() => {
+      spinner.classList.remove('visible')
+      firstDate.disabled = false;
+      secondDate.disabled = false;
+      submitBtn.disabled = false;
+      alert("Ошибка сервера");
+      
+    });
 });
 
 function getArrayOfDates(startDate, finishDate) {
@@ -92,9 +98,6 @@ function getArrayOfDates(startDate, finishDate) {
     startDate += DAY_IN_MILISECONDS;
   }
   return arrOfDates;
-  // let start =
-
-  // 24 * 60 * 60 * 1000 - сутки в милисекундах
 }
 
 function convertFromMilisecondsToDate(mSec) {
@@ -104,8 +107,7 @@ function convertFromMilisecondsToDate(mSec) {
   return `${year}-${correctDate(month)}-${correctDate(day)}`;
 }
 
-function createText(obj, parent){
-    let p = parent.querySelector('.date_value')
-    p.innerHTML = `${Object.keys(obj)[0]} : ${obj[Object.keys(obj)[0]]}`
-    
+function createText(obj, parent) {
+  let p = parent.querySelector(".date_value");
+  p.innerHTML = `${Object.keys(obj)[0]} : ${obj[Object.keys(obj)[0]]}`;
 }
